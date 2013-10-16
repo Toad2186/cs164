6496aaaa110b8b326b3821bfc2353a3fe1a8973c
eugene <elhuang@berkeley.edu>
2013-10-15 17:26:50 -0700

    fixed multi line longstring bug

--- proj1/tests/correct/def_docstrings.py
+++ proj1/tests/correct/def_docstrings.py
@@ -1,9 +1,7 @@
 # NOTE: DO DOCSTRINGS APPEAR IN THE AST? IDK.
 
-
 # Per Piazza @125 "Docstrings are long strings, and therefore expressions, so
 # just treat them as lone statements.
-
 def hello():
     """
     This is a doc string.
